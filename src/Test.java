da665942eb41fc60e9386d7daf2707e50a620b5f
tjtjdnjswhd <tjtjdnjswhd@gmail.com>
2024-05-30 09:34:41 +0900

    init

---
+++ src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    /**
+     * @author 아이우에오
+     * @apiNote 기능 설명
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
