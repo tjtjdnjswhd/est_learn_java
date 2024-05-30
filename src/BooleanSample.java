b991ce042511339ea1f55ed2beab527b3c946e99
tjtjdnjswhd <tjtjdnjswhd@gmail.com>
2024-05-30 15:59:37 +0900

    샘플 코드

---
+++ src/BooleanSample.java
@@ -0,0 +1,11 @@
+public class BooleanSample {
+    public static void main(String[] args) {
+        boolean isTrue = true;
+
+        if (isTrue) {
+            System.out.println("트루");
+        } else {
+            System.out.println("펄스");
+        }
+    }
+}
--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,17 @@
 public class Test {
-    /**
-     * @author 아이우에오
-     * @apiNote 기능 설명
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+  public static void main(String[] args) {
+    char c0 = '가';
+    // 유니코드 지원
+    char c1 = '\uac00';
+    int unicode = c1;
+    System.out.println(c0);
+    System.out.println(c1);
+    System.out.println(unicode);
+
+    float val = 3.14f;
+    float val1 = val * 2;
+
+    System.out.println(val);
+    System.out.println(val1);
+  }
 }
