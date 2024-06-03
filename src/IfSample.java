eae60f2d26b7b971af85d405f6d19bfa07232043
tjtjdnjswhd <tjtjdnjswhd@gmail.com>
2024-06-03 15:08:20 +0900

    IsSample

---
+++ src/IfSample.java
@@ -0,0 +1,63 @@
+public class IfSample {
+  public static void main(String[] args) {
+    //    int x = 0;
+    //    if (x > 0) {
+    //      System.out.println("x는 양수입니다.");
+    //    } else if (x < 0) {
+    //      System.out.println("x는 음수입니다.");
+    //    } else {
+    //      System.out.println("x는 0입니다.");
+    //    }
+    //
+    //    System.out.println(x > 0 ? "x는 양수입니다." : x < 0 ? "x는 음수입니다." : "x는 0입니다.");
+    //
+    //    int score = 75;
+    //    if (score >= 90) {
+    //      System.out.println("A");
+    //    } else if (score >= 80) {
+    //      System.out.println("B");
+    //    } else if (score >= 70) {
+    //      System.out.println("C");
+    //    } else if (score >= 60) {
+    //      System.out.println("D");
+    //    } else {
+    //      System.out.println("F");
+    //    }
+    //
+    //    int year = 2023;
+    //    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+    //      System.out.println("윤년입니다.");
+    //    } else {
+    //      System.out.println("윤년이 아닙니다.");
+    //    }
+    //
+    //    int a = 10;
+    //    int b = 20;
+    //    int c = 30;
+    //
+    //    if (a > b) {
+    //      if (a > c) {
+    //        System.out.println(a);
+    //      } else {
+    //        System.out.println(c);
+    //      }
+    //    } else {
+    //      if (b > c) {
+    //        System.out.println(b);
+    //      } else {
+    //        System.out.println(c);
+    //      }
+    //    }
+    //
+    //    int passScore = 60;
+    //    int myScore = 75;
+    //
+    //    if (myScore >= passScore) {
+    //      System.out.println("합격입니다.");
+    //    } else {
+    //      System.out.println("불합격입니다.");
+    //    }
+    //
+    //    System.out.println(myScore >= passScore ? "합격입니다." : "불합격입니다.");
+  }
+}
